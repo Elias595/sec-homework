f4eb2f717c8dfc3d1c7d04bf625fb832a7f3fee7
Elias <RedlE22004@sus.htl-grieskirchen.at>
2024-09-27 20:17:34 +0200

    Klasse NumberTester

--- sec-homework/src/NumberTester.java
+++ sec-homework/src/NumberTester.java
@@ -1,15 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class NumberTester {
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
+    private String fileName;
+
+
     public NumberTester(String fileName) {
-        //TODO
+        this.fileName = fileName;
     }
 
     public void setOddEvenTester(NumberTest oddTester) {
         this.oddTester = oddTester;
     }
-
-    HTL-Grieskirchen
-    Seite 1von 4
-    Hausübung2 POS1 3[A,C]
 
     public void setPrimeTester(NumberTest primeTester) {
         this.primeTester = primeTester;
@@ -20,6 +26,56 @@ public class NumberTester {
     }
 
     public void testFile() {
-        //TODO
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            int numberOfTests = Integer.parseInt(reader.readLine());
+
+            for (int i = 0; i < numberOfTests; i++) {
+                String[] parts = reader.readLine().split(" ");
+                int testType = Integer.parseInt(parts[0]);
+                int number = Integer.parseInt(parts[1]);
+
+                switch (testType) {
+                    case 1:
+                        if (oddTester.testNumber(number)) {
+                            System.out.println("EVEN");
+                        } else {
+                            System.out.println("ODD");
+                        }
+                        break;
+                    case 2:
+                        if (primeTester.testNumber(number)) {
+                            System.out.println("PRIME");
+                        } else {
+                            System.out.println("NO PRIME");
+                        }
+                        break;
+                    case 3:
+                        if (palindromeTester.testNumber(number)) {
+                            System.out.println("PALINDROME");
+                        } else {
+                            System.out.println("NO PALINDROME");
+                        }
+                        break;
+                    default:
+                        System.out.println("UNKNOWN TEST");
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+    static boolean isPalindrome(int number) {
+        String str = Integer.toString(number);
+        return str.equals(new StringBuilder(str).reverse().toString());
     }
 }
