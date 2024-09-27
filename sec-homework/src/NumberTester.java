c607cb7ecb8bc13066579f927ad7a104ed552b4c
Elias <RedlE22004@sus.htl-grieskirchen.at>
2024-09-27 19:45:05 +0200

    Klasse NumberTest

--- sec-homework/src/NumberTester.java
+++ sec-homework/src/NumberTester.java
@@ -1,3 +1,25 @@
 public class NumberTester {
+    public NumberTester(String fileName) {
+        //TODO
+    }
 
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
+
+    HTL-Grieskirchen
+    Seite 1von 4
+    Hausübung2 POS1 3[A,C]
+
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile() {
+        //TODO
+    }
 }
