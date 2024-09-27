b92e27fbe247064eea05975733bac4439efa8060
Elias <RedlE22004@sus.htl-grieskirchen.at>
2024-09-27 20:17:50 +0200

    Klasse Main

--- sec-homework/src/Main.java
+++ sec-homework/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        NumberTester tester = new NumberTester("numbers.txt");
 
+        tester.setOddEvenTester(number -> number % 2 == 0);
+        tester.setPrimeTester(NumberTester::isPrime);
+        tester.setPalindromeTester(NumberTester::isPalindrome);
+
+        tester.testFile();
     }
 }
+
